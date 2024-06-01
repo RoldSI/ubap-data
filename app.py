bb9e08ea2e2d19cbd985709389065ae228687286
Simon Sure <info@simonsure.com>
2024-06-01 14:40:22 +0200

    [data retrival] report, image, uxv status
    Signed-off-by: Simon Sure <info@simonsure.com>

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
 load_dotenv()
@@ -23,19 +24,95 @@ def hello():
     return 'Hello, World! This is the UBAP interface to interact with autonomous vehicles.'
 
 @app.route('/submit/report', methods=['POST'])
-def submit_text():
+def submit_report():
     # Get the JSON data from the request
     data = request.get_json()
-    if not data or 'text' not in data:
-        return jsonify({"error": "No text provided"}), 400
+    if not data or 'text' not in data or 'latitude' not in data or 'longitude' not in data or 'source' not in data:
+        return jsonify({"error": "Not all fields provided"}), 400
     
     # Get the text from the JSON data
     text = data['text']
+    longitude = data['longitude']
+    latitude = data['latitude']
+    source = data['source']
     
     # Create the document
     document = {
         "type": "report",
-        "text": text
+        "source": source,
+        "timestamp": datetime.now(),
+        "location": {
+            "longitude": longitude,
+            "latitude": latitude,
+        },
+        "text": text,
+    }
+
+    # Insert the document into the collection
+    result = collection.insert_one(document)
+    
+    # Return a success response
+    return jsonify({"message": "Text stored successfully", "id": str(result.inserted_id)}), 201
+
+@app.route('/submit/uxv', methods=['POST'])
+def submit_uxv():
+    # Get the JSON data from the request
+    data = request.get_json()
+    if not data or 'latitude' not in data or 'longitude' not in data or 'goal_latitude' not in data or 'goal_longitude' not in data or 'uxv_id' not in data:
+        return jsonify({"error": "Not all fields provided"}), 400
+    
+    # Get the text from the JSON data
+    longitude = data['longitude']
+    latitude = data['latitude']
+    goal_longitude = data['goal_longitude']
+    goal_latitude = data['goal_latitude']
+    uxv_id = data['uxv_id']
+    
+    # Create the document
+    document = {
+        "type": "uxv",
+        "uxv_id": uxv_id,
+        "timestamp": datetime.now(),
+        "location": {
+            "longitude": longitude,
+            "latitude": latitude
+        },
+        "goal": {
+            "longitude": longitude,
+            "latitude": latitude
+        },
+    }
+
+    # Insert the document into the collection
+    result = collection.insert_one(document)
+    
+    # Return a success response
+    return jsonify({"message": "Text stored successfully", "id": str(result.inserted_id)}), 201
+
+@app.route('/submit/image', methods=['POST'])
+def submit_image():
+    # Get the JSON data from the request
+    data = request.get_json()
+    if not data or 'latitude' not in data or 'longitude' not in data or 'url' not in data or 'uxv_id' not in data:
+        return jsonify({"error": "No text provided"}), 400
+    
+    # Get the text from the JSON data
+    text = data['url']
+    longitude = data['longitude']
+    latitude = data['latitude']
+    url = data['url']
+    uxv_id = data['uxv_id']
+    
+    # Create the document
+    document = {
+        "type": "image",
+        "uxv_id": uxv_id,
+        "timestamp": datetime.now(),
+        "location": {
+            "longitude": longitude,
+            "latitude": latitude
+        },
+        "url": url,
     }
 
     # Insert the document into the collection
